a17155868264b19edf8276fd99a6537ab68177e7
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-13 13:12:57 -0500

    Updated JS

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ var endYear = $("#endYear").val();
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 url += '?' + $.param({
   'api-key': "60064f02c0cb48578b7deb817fa16075",
-  'q': "cats"
+  'q': searchTerm,
+  'begin_date': "20170221",
+  'end_date': "20170513",
+  'sort': "newest",
+  'page': 0
+
 });
 $.ajax({
   url: url,
@@ -26,7 +31,5 @@ $.ajax({
 });
 
 
-
-})
 
 })
