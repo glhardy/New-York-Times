c7064141a8dbb60eb0072b25ace4f5e3c97df840
Adam Kirschner <akirschner_atx@Adams-MacBook-Air.local>
2017-05-13 13:52:32 -0500

    Updated JS

--- app.js
+++ app.js
@@ -1,47 +1,52 @@
 $(document).ready(function() {
 
-var articleInfoDiv = $("<div>");
+    var articleInfoDiv = $("<div>");
 
     $("#submit").on("click", function() {
 
-        event.preventDefault();
+            event.preventDefault();
 
-        var searchTerm = $("#searchTerm").val();
-        var retrieveThisMany = $("#retrieveThisMany").val();
-        var startYear = $("#startYear").val();
-        var endYear = $("#endYear").val();
+            var searchTerm = $("#searchTerm").val();
+            var retrieveThisMany = $("#numRecordsSelect").val();
+            var startYear = $("#startYear").val();
+            var endYear = $("#endYear").val();
 
 
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-            'api-key': "60064f02c0cb48578b7deb817fa16075",
-            'q': searchTerm,
-            'begin_date': "20170221",
-            'end_date': "20170513",
-            'sort': "newest",
-            'page': 0
+            var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+            url += '?' + $.param({
+                'api-key': "60064f02c0cb48578b7deb817fa16075",
+                'q': searchTerm,
+                'begin_date': "20170221",
+                'end_date': "20170513",
+                'sort': "newest",
+                'page': 0
 
-        });
-        $.ajax({
-            url: url,
-            method: 'GET',
-        }).done(function(result) {
-        	console.log(result);
-            console.log(result.response.docs[0].headline.main);
-            console.log(result.response.docs[0].byline.original);
-            console.log("Section: " + result.response.docs[0].subsection_name);
-            console.log(result.response.docs[0].pub_date);
-            console.log("Section: " + result.response.docs[0].web_url);
+            });
+            $.ajax({
+                url: url,
+                method: 'GET',
+            }).done(function(result) {
+                    console.log(result);
+                    console.log(result.response.docs[0].headline.main);
+                    console.log(result.response.docs[0].byline.original);
+                    console.log("Section: " + result.response.docs[0].subsection_name);
+                    console.log(result.response.docs[0].pub_date);
+                    console.log("Section: " + result.response.docs[0].web_url);
+           
 
-            articleInfoDiv.append("<h4>" + result.response.docs[0].headline.main + "</h4>");
-            articleInfoDiv.append("<h6>" + result.response.docs[0].byline.original + "</h6>");
-            articleInfoDiv.append("<h6> Section: " + result.response.docs[0].subsection_name + "</h6>");
-            articleInfoDiv.append("<h6>" + result.response.docs[0].pub_date + "</h6>");
-            articleInfoDiv.append("<h6> Section: " + result.response.docs[0].web_url + "</h6>");
+                    for (i = 0; i < retrieveThisMany; i++) {
+                    	articleInfoDiv.append("<h4>" + result.response.docs[i].headline.main + "</h4>");
+                        articleInfoDiv.append("<h6>" + result.response.docs[i].byline.original + "</h6>");
+                        articleInfoDiv.append("<h6> Section: " + result.response.docs[i].subsection_name + "</h6>");
+                        articleInfoDiv.append("<h6>" + result.response.docs[i].pub_date + "</h6>");
+                        articleInfoDiv.append("<h6> Section: " + result.response.docs[i].web_url + "</h6>");
 
-            $(".articleDiv").append(articleInfoDiv);
+                        $(".articleDiv").append(articleInfoDiv);
+                    };
+                    
+              
 
-        }).fail(function(err) {
+            }).fail(function(err) {
             throw err;
         });
 
